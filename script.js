4b99a05e44a75170524640693daa93a24d014bd0
Steven Friend <stevenmarkfriend@gmail.com>
2024-02-17 23:49:05 +0900

    Add FPS counter

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-const canvas = document.querySelector('canvas')
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext("2d");
+
+let frames, framet;
+const fpsDislpay = document.querySelector('#fps')
 
 window.addEventListener('resize', () => {
-  canvas.width = innerWidth
-  canvas.height = innerHeight
-})
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+});
+
+const init = () => {
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+  frames = 0;
+  framet = performance.now();
+  requestAnimationFrame(animate);
+}
+
+const animate = timestamp => {
+  frames++
+
+  if(timestamp - framet >= 1000) {
+    fpsDislpay.innerHTML = `FPS: ${frames}`;
+    framet = timestamp;
+    frames = 0;
+  }
+
+  myRequest = requestAnimationFrame(animate);
+}
+
+init()
